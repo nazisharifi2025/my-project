a327eb58093ef6ca6e4efb6d2534cc70a622891a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-20 21:18:49 +0430

    l'

---
+++ Dashbord-D/app.js
@@ -0,0 +1,19 @@
+
+// تنظیمات:
+const start = 300;
+const end = 560;
+const duration = 5000; // به میلی‌ثانیه (5 ثانیه)
+const counterElement = document.getElementById("counter");
+const range = end - start;
+const steps = 100; // چند مرحله (برای نرم بودن)
+const stepTime = duration / steps;
+let current = start;
+let step = range / steps;
+const timer = setInterval(() => {
+  current += step;
+  if (current >= end) {
+    current = end;
+    clearInterval(timer);
+  }
+  counterElement.textContent = Math.round(current);
+}, stepTime);
